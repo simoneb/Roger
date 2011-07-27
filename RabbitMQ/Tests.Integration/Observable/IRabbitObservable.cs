66121f754835776261239176cf7dd913fbdda747
Simone Busoli <simone.busoli@gmail.com>
2011-07-27 11:49:27 +0000

    Removed unlucky spike

--- RabbitMQ/Tests.Integration/Observable/IRabbitObservable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Tests.Integration.Observable
-{
-    public interface IRabbitObservable : IObservable<RabbitMessage>
-    {
-        IDisposable Subscribe(IObserver<RabbitMessage> observer, QueueOptions queue = default(QueueOptions));
-    }
-}
