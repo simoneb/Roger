f5f4a12c5b4ff384c58de3da47e1f0e65a1aa70d
Simone Busoli <simone.busoli@gmail.com>
2012-01-09 14:31:07 +0100

    Don't set environment variable if already defined

--- RabbitMQServer/config/startup-config.bat
+++ RabbitMQServer/config/startup-config.bat
@@ -1,6 +1,9 @@
 echo Initializing custom config...
 
-set ERLANG_HOME=!PROGRAMFILES(x86)!\erl5.8.3\erts-5.8.3
+if "!ERLANG_HOME!"=="" (
+set "ERLANG_HOME=!PROGRAMFILES(x86)!\erl5.8.5\erts-5.8.5"
+)
+
 set ERLANG_SERVICE_MANAGER_PATH=!ERLANG_HOME!\bin
 set RABBITMQ_BASE=%~dp0..\data
 set RABBITMQ_CONFIG_FILE=%~dp0rabbitmq
