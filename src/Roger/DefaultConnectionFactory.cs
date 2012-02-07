bf2cc34c48285656deb989ab07dd6eaeb90b1366
Simone Busoli <simone.busoli@gmail.com>
2012-02-07 11:57:54 +0100

    Added heartbeat option

--- src/Roger/DefaultConnectionFactory.cs
+++ src/Roger/DefaultConnectionFactory.cs
@@ -11,16 +11,17 @@ namespace Roger
         /// Creates a new instance of <see cref="DefaultConnectionFactory"/>
         /// </summary>
         /// <param name="uri">A <see cref="Uri"/> in the form <example>amqp://user:pass@host:10000/vhost</example> as specified in <c>http://www.rabbitmq.com/uri-spec.html</c></param>
-        public DefaultConnectionFactory(Uri uri)
+        public DefaultConnectionFactory(Uri uri, ushort heartbeatInSeconds = ConnectionFactory.DefaultHeartbeat)
         {
-            inner = new ConnectionFactory {uri = uri};
+            inner = new ConnectionFactory {uri = uri, RequestedHeartbeat = heartbeatInSeconds};
         }
 
         public DefaultConnectionFactory(string hostName,
                                         string virtualHost = ConnectionFactory.DefaultVHost,
                                         int port = 5672,
                                         string userName = ConnectionFactory.DefaultUser,
-                                        string password =  ConnectionFactory.DefaultPass)
+                                        string password =  ConnectionFactory.DefaultPass,
+                                        ushort heartbeatInSeconds = ConnectionFactory.DefaultHeartbeat)
         {
             inner = new ConnectionFactory
             {
@@ -28,7 +29,8 @@ namespace Roger
                 VirtualHost = virtualHost,
                 Port = port,
                 UserName = userName,
-                Password = password
+                Password = password,
+                RequestedHeartbeat = heartbeatInSeconds
             };
         }
 
